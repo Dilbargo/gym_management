7db7d6fd5d208fce69fb1d54e6cc1f3f69e99d57
Dilbar <dilbar_m005@mail.ru>
2026-05-13 23:05:26 +0500

    add exception

---
+++ src/main/java/com/gym/gym_management/exception/DilbarNotFoundException.java
@@ -0,0 +1,7 @@
+package com.gym.gym_management.exception;
+
+public class DilbarNotFoundException extends RuntimeException {
+    public DilbarNotFoundException(String message) {
+        super(message);
+    }
+}
